c58a920776ab484da7d35796679c34ca87d10454
DarkWiiplayer <darkwiiplayer@hotmail.com>
2019-10-02 15:49:07 +0200

    Implement RGB to HSL converter function

--- colorspace.lua
+++ colorspace.lua
@@ -6,6 +6,18 @@ local function clamp(a, min, max)
 	return math.min(max, math.max(min, a))
 end
 
+local function nmax(fn, ...)
+  local n, max = 0, -fn(-math.huge, math.huge)
+  for i=1,select('#', ...) do
+    local value = select(i, ...)
+    if value==fn(value, max) then
+      max = value
+      n = i
+    end
+  end
+  return n, max
+end
+
 --- Converts from HSL colorspace to RGB colorspace.
 -- Additional arguments are passed through,
 -- meaning this function doubles as a hsla2rgba converter.
@@ -31,6 +43,39 @@ function colorspace.hsl2rgb(h, s, l, ...)
 	end
 
 	return r+m, g+m, b+m, ...
+end
+
+--- Converts from RGB colorspace to HSL colorspace.
+-- Additional arguments are passed through,
+-- meaning this function doubles as a rgba2hsla converter.
+function colorspace.rgb2hsl(r, g, b, ...)
+  local h, s, l
+  r, g, b = clamp(r, 0, 1), clamp(g, 0, 1), clamp(b, 0, 1)
+  local imax, max = nmax(math.max, r, g, b)
+  local    _, min = nmax(math.min, r, g, b)
+  local delta = max-min
+
+  l = (max + min) / 2
+
+  if delta == 0 then
+    h = 0
+  elseif imax == 1 then
+    h = 60 * ((g-b) / delta % 6)
+  elseif imax == 2 then
+    h = 60 * ((b-r) / delta + 2)
+  elseif imax == 3 then
+    h = 60 * ((r-g) / delta + 4)
+  else
+    error('this code should be unreachable')
+  end
+
+  if delta > 0 then
+    s = delta / (1 - math.abs(2*l - 1))
+  else
+    s = 0
+  end
+
+  return h, s, l, ...
 end
 
 return colorspace
--- spec/rgb2hsl_spec.moon
+++ spec/rgb2hsl_spec.moon
@@ -1,9 +1,9 @@
 colorspace = require 'colorspace'
 colors     = require 'spec/vectors'
 
-pending 'rgb2hsl', ->
+describe 'rgb2hsl', ->
 	for color in *colors
-		pending "Should correctly convert #{color.name} (#{color.hex}, hsl(#{color.hsl[1]}, #{color.hsl[2]}, ##{color.hsl[3]}))", ->
+		it "Should correctly convert #{color.name} (#{color.hex}, hsl(#{color.hsl[1]}deg, #{color.hsl[2]*100}%, #{color.hsl[3]*100}%))", ->
 			assert.same color.hsl, {colorspace.rgb2hsl(color.rgb[1], color.rgb[2], color.rgb[3])}
 	it "Should just pass additional arguments through", ->
 		-- aka it should double as a hsla2rgba converter
